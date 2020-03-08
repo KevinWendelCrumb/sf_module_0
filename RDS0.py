f9f6c370a02b790eb62e6d4d02966cc564abc762
Lesia Danilina <lesia.feliz@gmail.com>
2020-03-08 18:22:49 +0500

    новый код исправленный, работает

--- RDS0.py
+++ RDS0.py
@@ -1,23 +1,33 @@
-import random
+import numpy as np
 from random import randint
-number = random.randint(1,100)
-computer_number = 50
-tries = 1
-low = 1
-high = 100
-# Цикл отгадывания
-while computer_number != number:
-    if computer_number > number:
-        high = computer_number    # Задаем загаданное число верхней границей интервала
-# Продолжаем делить полученный интервал наполовину.
-        computer_number = computer_number - ((high-low)//2)
-        print(computer_number)
-    elif computer_number < number:
-        low = computer_number    # Задаем загаданное число нижней границей интервала
-        computer_number = computer_number + ((high-low)//2)
-        print(computer_number)
-        tries += 1
-     
-print("Компьютер потратил", tries, "попытки(ок) на отгадывание числа.")
 
+def random_finder(number, predict = 50, tries = 1):
+    range = [1, 101]
+    # Цикл отгадывания
+    while predict != number:
+        tries += 1
+        if predict < number:
+            range[0] = predict    # Задаем загаданное число нижней границей интервала
+    # Продолжаем делить полученный интервал наполовину.
+            predict = int((predict + range[1]) / 2)
+        elif predict > number:
+            range[1] = predict    # Задаем загаданное число верхней границей интервала
+            predict = int((predict + range[0]) / 2)
+    return tries
+    print("Компьютер потратил", tries, "попытки(ок) на отгадывание числа.")
+    
+def score_game(random_finder):
+    '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(random_finder(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return (score)
+
+
+# запускаем
+score_game(random_finder)
 
